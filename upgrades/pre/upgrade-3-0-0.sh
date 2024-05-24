07b79bd5d26444b2395bc2033484c383df395ebb
Matthias Erll <merll@akamai.com>
2024-05-24 10:28:50 +0200

    fix: run upgrade conditionally

--- upgrades/pre/upgrade-3-0-0.sh
+++ upgrades/pre/upgrade-3-0-0.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
-kubectl annotate -n gitea secret/gitea-postgresql helm.sh/resource-policy='keep' deprecated=true
-kubectl annotate -n gitea sts/gitea-postgresql helm.sh/resource-policy='keep' deprecated=true
-kubectl annotate -n gitea svc/gitea-postgresql helm.sh/resource-policy='keep' deprecated=true
+if kubectl get -n gitea secret | grep gitea-postgresql; then
+  kubectl annotate -n gitea secret/gitea-postgresql helm.sh/resource-policy='keep' deprecated=true
+  kubectl annotate -n gitea sts/gitea-postgresql helm.sh/resource-policy='keep' deprecated=true
+  kubectl annotate -n gitea svc/gitea-postgresql helm.sh/resource-policy='keep' deprecated=true
+fi
